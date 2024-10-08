59f0d10e45da249a2f7db83d911ed10def5e6cd3
Vijay Krishna <vijaykrishnay07@gmail.com>
2024-10-07 19:46:08 -0700

    create signup form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    mobile: "",
+    password: "",
+    confirmpassword: "",
+  });
+
+  const { username, email, mobile, password, confirmpassword } = data;
+
+  const changeHandler = (e) => {
+    setData({ ...data, [e.target.name]: [e.target.value] });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(data);
+    setData({
+      username: "",
+      email: "",
+      mobile: "",
+      password: "",
+      confirmpassword: "",
+    });
+    alert("Form Submitted");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <center>
+        <h1>Welcome World!!</h1>
+        <form onSubmit={submitHandler}>
+          <label>Username : </label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="username"
+            value={username}
+            onChange={changeHandler}
+          />
+          <br />
+          <label>Email Address : </label>
+          <input
+            type="email"
+            placeholder="Enter Email address"
+            name="email"
+            value={email}
+            onChange={changeHandler}
+          />
+          <br />
+          <label>Mobile Number : </label>
+          <input
+            type="number"
+            placeholder="Enter Mobile number"
+            name="mobile"
+            value={mobile}
+            onChange={changeHandler}
+          />
+          <br />
+          <label>Password : </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            value={password}
+            onChange={changeHandler}
+          />
+          <br />
+          <label>Confirm Password : </label>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmpassword"
+            value={confirmpassword}
+            onChange={changeHandler}
+          />
+          <br />
+          <br />
+          <input type="submit" name="Submit Form" />
+        </form>
+      </center>
+    </>
   );
-}
+};
 
 export default App;
